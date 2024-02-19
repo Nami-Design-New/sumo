4d531311899b35f171e592a807c386bd0740126d
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-02-19 19:13:18 +0200

    #first

--- js/custom.js
+++ js/custom.js
@@ -327,3 +327,11 @@ $(document).ready(function () {
   const elements = document.querySelectorAll(".counterUp");
   elements.forEach((el) => IO.observe(el));
 });
+function highlight(el) {
+  el.previousElementSibling.classList.add("h");
+}
+function dehighlight(el) {
+  if (el.value === "") {
+    el.previousElementSibling.classList.remove("h");
+  }
+}
